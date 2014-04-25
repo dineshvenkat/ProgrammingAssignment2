2f04a40b5653f370c247b376cff8e332597855cc
unknown <dvenkataraman@dvenkataraman.india.wirelessworld.airvananet.com>
2014-04-25 10:22:39 +0530

    Updated with actual code for assignement evaluation

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix function takes matrix as an input and returns a special list
+# with function objects. Function objects in general are used to manipulate
+# (e.g get, set etc) on the input matrix  and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL   #cache variable used to store the inverse of matrix. Default NULL
+  
+  #Set the internal variable with the original input marix 
+  set <- function(y) {  
+    x <<- y
+    inv <<- NULL
+  }
+  
+  #Return the original matrix
+  get <- function() x
+  
+  #Cache the matrix inverse 
+  setinv <- function(inverse) inv <<- inverse
+  
+  #Return the  cached matrix inverse 
+  getinv <- function() inv
+  
+  #Prepare and return the special list with function objects
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function take the  list returned by makeCacheMatrix as input.
+## It returns the inv of matrix if value is already cached and available. otherwise 
+## this function computes the inverse of matrix , stores the value in the cache 
+## and returns the inverse of matrix. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+  inv <- x$getinv()
+  
+  ## Check if cached already available 
+  if(!is.null(inv)) {
+    message("Getting  matrix inv from cached data")
+  
+    ## Return from cache - a matrix that is the inverse of 'x'
+    return(inv)
+  }
+  
+  ##Compute the inverse of matrix if cached value not available
+  mat <- x$get()
+  inv <- solve(mat)
+  
+  ##Cached the computed inverse 
+  x$setinv(inv)
+  
+  ## Return the computed the inverse of matrix
+  inv
 }
